eb19706555d06b4c0a07ad3b68879cd1c8adef99
guilhermerc <guilherme.riciolic@gmail.com>
2018-03-11 01:11:49 -0300

    Removendo a pasta do gnu octave que tinha espaço no nome

--- GNU Octave/getCalibrationParameters.m
+++
@@ -1,9 +0,0 @@
-% Returns de array of parameters of adjustment
-
-function parameterArray = getCalibrationParameters(array)
-  pixel = 1;
-  MAX_WHITE = 255;
-  while (pixel <= 128)
-    parameterArray(pixel) = MAX_WHITE - array(pixel);
-    pixel++;
-  endwhile
