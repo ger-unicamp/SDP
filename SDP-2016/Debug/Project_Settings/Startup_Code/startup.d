9cc8ebc910fbbafd83c30039746a01463c4ea059
guilhermerc <guilherme.riciolic@gmail.com>
2018-04-09 13:58:08 -0300

    Subindo depois de dar make clean

--- SDP-2016/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- /home/guilherme/Documents/SDP/SDP-2016/Generated_Code/IO_Map.h \
- /home/guilherme/Documents/SDP/SDP-2016/Static_Code/IO_Map/MKL25Z4.h
-
-/home/guilherme/Documents/SDP/SDP-2016/Generated_Code/IO_Map.h:
-
-/home/guilherme/Documents/SDP/SDP-2016/Static_Code/IO_Map/MKL25Z4.h:
