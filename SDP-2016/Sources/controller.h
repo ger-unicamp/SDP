361c21f0115a98afc4ea2d45c2791a2808b4d5e7
Luis Felipe Brentegani <lf.brentegani@gmail.com>
2018-11-09 17:36:16 -0200

    Changes due refactoring of code

---
+++ SDP-2016/Sources/controller.h
@@ -0,0 +1,13 @@
+#ifndef CONTROLLER_H_ //Starting the condition to define library
+#define CONTROLLER_H_
+
+
+#include <camera_lib.h>
+#include <dc_motor_lib.h>
+#include <servo_lib.h>
+#include "PE_Types.h"
+
+
+void controller(double pid_out);
+
+#endif /* CONTROLLER_H_ */ //Ending the definition
